d8dd2654739e64eed7240109968e0a9b35e15385
Jung ho Jeon <wjswjdgh03@gmail.com>
2024-01-13 21:34:10 +0900

    이미지 업로드

--- src/main/java/SimpleBoard/sb/web/BoardController.java
+++ src/main/java/SimpleBoard/sb/web/BoardController.java
@@ -6,12 +6,19 @@ import SimpleBoard.sb.service.BoardService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,9 +39,43 @@ public class BoardController {
     }
 
     @PostMapping("/createPost")
-    public String createPost(@ModelAttribute Board board) {
-        System.out.println(board);
-        boardService.save(board);
+    public String createPost(@ModelAttribute Board board, @RequestParam(required = false,value = "image") MultipartFile image) {
+        FTPClient ftp = new FTPClient();
+        String extension = StringUtils.getFilenameExtension(image.getOriginalFilename());
+        ftp.setControlEncoding("utf-8");
+        String filename = image.getOriginalFilename();
+        FTPClientConfig ftpConfig = new FTPClientConfig();
+        UUID uuid = UUID.randomUUID();
+        if(image.getSize()==0){
+            boardService.save(board);
+            return "redirect:/board/boardList";
+        } else{
+            try{
+                InetAddress inet = Inet4Address.getLocalHost();
+                String ip = inet.getHostAddress();
+                String host = "iup.cdn1.cafe24.com";
+                String user = "wjswjdgh123";
+                String pass = "qlalfqjsgh1@";
+                int port = 21;
+                ftp.configure(ftpConfig);
+                ftp.connect(host, port);
+                if(ftp.login(user,pass)){
+                    ftp.setFileType(FTP.BINARY_FILE_TYPE);
+                    int replyCode = ftp.getReplyCode();
+                    boolean result = ftp.storeFile("/www/img/" + uuid + "." + extension, image.getInputStream());
+                    if(result){
+                        board.setImageUrl("http://wjswjdgh123.cdn1.cafe24.com/img/"+uuid+"."+extension);
+                        board.setImageName(filename);
+                        boardService.save(board);
+                        return "redirect:/board/boardList";
+                    }else{
+                        throw new IllegalStateException();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return "redirect:/board/boardList";
     }
 }
